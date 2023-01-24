8a6bfc484af3949666368e3da85a013719581117
bodwood <bodiewood@hotmail.com>
2023-01-24 11:21:33 -0800

    make navbar responsive

--- SearchCoach/Migrations/20230124180424_Initial.Designer.cs
+++ SearchCoach/Migrations/20230124180424_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SearchCoach.Models;
 
@@ -10,9 +11,10 @@ using SearchCoach.Models;
 namespace SearchCoach.Migrations
 {
     [DbContext(typeof(SearchCoachContext))]
-    partial class SearchCoachContextModelSnapshot : ModelSnapshot
+    [Migration("20230124180424_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
