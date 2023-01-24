43d06a12a848dd242fa73950aa127e6b9a9f6410
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-24 11:15:53 -0800

    Add authentication w. Identity + assoc Acct Views
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- SearchCoach/Migrations/20230124185525_AddAuthentication.Designer.cs
+++ SearchCoach/Migrations/20230124185525_AddAuthentication.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SearchCoach.Models;
 
@@ -10,9 +11,10 @@ using SearchCoach.Models;
 namespace SearchCoach.Migrations
 {
     [DbContext(typeof(SearchCoachContext))]
-    partial class SearchCoachContextModelSnapshot : ModelSnapshot
+    [Migration("20230124185525_AddAuthentication")]
+    partial class AddAuthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
