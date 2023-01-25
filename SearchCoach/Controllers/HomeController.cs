09405304b908099508cf82305cece3558ec10670
bodwood <bodiewood@hotmail.com>
2023-01-25 15:25:02 -0800

    add style to all h1 tags

--- SearchCoach/Controllers/HomeController.cs
+++ SearchCoach/Controllers/HomeController.cs
@@ -7,80 +7,108 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Claims;
 using System;
+using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
 
 namespace SearchCoach.Controllers
 {
+  [Authorize]
   public class HomeController : Controller
   {
     private readonly SearchCoachContext _db;
-    // private readonly UserManager<ApplicationUser> _userManager;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-     public HomeController(SearchCoachContext db)
-      {
-        _db = db;
-      }
-    public ActionResult Index()
+    public HomeController(UserManager<ApplicationUser> userManager, SearchCoachContext db)
     {
-      // 
-      // Stats!
-      Dictionary<string, int> stats = new Dictionary<string, int>();
+      _userManager = userManager;
+      _db = db;
+    }
 
-      // WeeklyAppAvg = total app count / total weeks
-      // get total app count
-      // int AppCount = _db.Applications.Count();
-      // double AppCountToDouble = System.Convert.ToDouble(AppCount); // get count of all apps 1
-      // // get total days count
-      // double dateNow = DateTime.Now.ToOADate(); // get current date 1/24/22
-      // Application FirstApp = _db.Applications.FirstOrDefault(model => model.ApplicationId == 1); // get date of first app ever submitted 1/23/22
-      // double dateOfFirstApp = FirstApp.Date.ToOADate(); // get date of first app ever submitted 1/23/22
-      // int elapsedDays = (int)(dateNow - dateOfFirstApp); // get elapsed days
-      // int elapsedWeeks = (elapsedDays / 7); if ((elapsedDays % 7) > 0) {elapsedWeeks++;} // get elapsed weeks
-      // // TODO: add test for above
-      //   // if elapsedDays == 0, then 0 wk
-      //   // if elapsedDays == 2, then 1 wk
-      //   // if elapsedDays == 21, then 3 wk
-      //   // if elapsedDays == 23, then 4 wk
-      // double WeeklyAppAvgDouble = (AppCountToDouble / elapsedWeeks);
-      // int WeeklyAppAvg = (int)WeeklyAppAvgDouble;
+    public async Task<ActionResult> Index()
+    {
+      // get logged in user
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
 
-      // stats.Add("WeeklyAppAvg", WeeklyAppAvg);
-
-      // // AllTimeAppCount = total app count
-      // int AllTimeAppCount = _db.Applications.Count();
-
-      // stats.Add("AllTimeAppCount", AllTimeAppCount);
-
-      // // AllTimeCompCount = total comp count
-      // int AllTimeCompCount = _db.Companies.Count();
-
-      // stats.Add("AllTimeCompCount", AllTimeCompCount);
-
-      // // AllTimePhoneCount = total phone screen count
-      // int AllTimePhoneScreen = _db.Applications
-      //                             .Include(model => model.Status)
-      //                             .Where(model => model.Status.PhoneScreen == true).Count();
-                                  
-      // stats.Add("AllTimePhoneScreen", AllTimePhoneScreen);
-
-      // // AllTimeInterview = total interview count
-
-      // int AllTimeInterview1  = _db.Applications
-      //                             .Include(model => model.Status)
-      //                             .Where(model => model.Status.Interview1 == true).Count();
-      // int AllTimeInterview2  = _db.Applications
-      //                             .Include(model => model.Status)
-      //                             .Where(model => model.Status.Interview2 == true).Count();
-                                
-      // int AllTimeInterview = AllTimeInterview1 + AllTimeInterview2;
-      // stats.Add("AllTimeInterview", AllTimeInterview);
-
-      Company[] companies = _db.Companies.ToArray();
-      Application[] applications = _db.Applications.Include(model => model.Status).ToArray();
+      IQueryable<Application> userApps = _db.Applications.Where(entry => entry.User.Id == currentUser.Id);
       Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      Company[] companies = _db.Companies
+                                      .Where(entry => entry.User.Id == currentUser.Id)
+                                      .ToArray();
+      Application[] applications = userApps.Include(model => model.Status).ToArray();
+        
       model.Add("companies", companies);
       model.Add("applications", applications);
-      ViewBag.Stats = stats;
-      return View(model);
+      if (userApps != null) {
+        // Stats!
+        Dictionary<string, int> stats = new Dictionary<string, int>();
+        // WeeklyAppAvg = total app count / total weeks
+        // get total app count
+        int AppCount = userApps.Count();
+        double AppCountToDouble = System.Convert.ToDouble(AppCount); // get count of all apps 1
+        // get total days count
+        double dateNow = DateTime.Now.ToOADate(); // get current date 1/24/22
+        Application FirstApp = userApps.FirstOrDefault(model => model.ApplicationId == 1); // get date of first app ever submitted 1/23/22
+        if (FirstApp != null)
+        {
+          double dateOfFirstApp = FirstApp.Date.ToOADate(); // get date of first app ever submitted 1/23/22
+          int elapsedDays = (int)(dateNow - dateOfFirstApp); // get elapsed days
+          int elapsedWeeks = (elapsedDays / 7); if ((elapsedDays % 7) > 0) {elapsedWeeks++;} // get elapsed weeks
+          // TODO: add test for above
+            // if elapsedDays == 0, then 0 wk
+            // if elapsedDays == 2, then 1 wk
+            // if elapsedDays == 21, then 3 wk
+            // if elapsedDays == 23, then 4 wk
+          double WeeklyAppAvgDouble = (AppCountToDouble / elapsedWeeks);
+          int WeeklyAppAvg = (int)WeeklyAppAvgDouble;
+
+          stats.Add("WeeklyAppAvg", WeeklyAppAvg);
+
+          // AllTimeAppCount = total app count
+          int AllTimeAppCount = userApps.Count();
+
+          stats.Add("AllTimeAppCount", AllTimeAppCount);
+
+          // TODO: connect to companies related to a given user
+          // AllTimeCompCount = total comp count
+          int AllTimeCompCount = _db.Companies
+                                            .Where(entry => entry.User.Id == currentUser.Id)
+                                            .Count();
+
+          stats.Add("AllTimeCompCount", AllTimeCompCount);
+
+          // AllTimePhoneCount = total phone screen count
+          int AllTimePhoneScreen = userApps
+                                      .Include(model => model.Status)
+                                      .Where(model => model.Status.PhoneScreen == true).Count();
+                                      
+          stats.Add("AllTimePhoneScreen", AllTimePhoneScreen);
+
+          // AllTimeInterview = total interview count
+
+          int AllTimeInterview1  = userApps
+                                      .Include(model => model.Status)
+                                      .Where(model => model.Status.Interview1 == true).Count();
+          int AllTimeInterview2  = userApps
+                                      .Include(model => model.Status)
+                                      .Where(model => model.Status.Interview2 == true).Count();
+                                    
+          int AllTimeInterview = AllTimeInterview1 + AllTimeInterview2;
+          stats.Add("AllTimeInterview", AllTimeInterview);
+          ViewBag.Stats = stats;
+
+          return View(model);
+        }
+        else
+        {
+          return View(model);
+        }
+      }
+      else 
+      {
+        return View(model);
+      }
+      
     }
 
     public ActionResult Search(string query)
---
+++ SearchCoach/Migrations/20230125222918_AddUserId.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SearchCoach.Migrations
+{
+    public partial class AddUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Companies",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_UserId",
+                table: "Companies",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_AspNetUsers_UserId",
+                table: "Companies",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_AspNetUsers_UserId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_UserId",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Companies");
+        }
+    }
+}
